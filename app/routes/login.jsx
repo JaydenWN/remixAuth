82d03df11085acc76300c5878c7b9732f7f69c80
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2023-08-28 19:17:44 +1000

    changed required sign in form to be email and password

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -1,12 +1,20 @@
 import { Form } from "@remix-run/react";
 
+export function action({ request }) {
+  const formdata = await request.formData();
+  const email = formdata.get("email");
+  const password = formdata.get("password");
+
+
+}
+
 export default function Login() {
   return (
     <>
       <h1>Login</h1>
-      <Form>
-        <label htmlFor="username">Username :</label>
-        <input type="text" id="username" name="username" />
+      <Form method="POST">
+        <label htmlFor="email">Email :</label>
+        <input type="text" id="email" name="email" />
 
         <label htmlFor="password">Password :</label>
         <input type="password" id="password" name="password" />
